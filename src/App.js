d64645c5dfd1953d99c94bc8f7cfc88a99e63d97
Ahmed Dghaies <dghais.ahmed7@gmail.com>
2020-10-22 18:15:59 +0100

    Adding main home parts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import Banner from './Banner';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return ( 
+
+    //BEM
+    <div className = "app">
+        { /* Home */}
+        <Header />
+            { /* Header */}
+
+            { /* Banner */}
+            <Banner /> 
+                { /* Search*/}
+
+            { /* Cards */}
+
+            { /* Footer */}
+        
+        { /* Search page*/}
+
+            { /* */}
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './search.css'
+
+function Search() {
+    return (
+        <div className='search'>
+            <h1>Date Picker</h1>
+        </div>
+    )
+}
+
+export default Search
